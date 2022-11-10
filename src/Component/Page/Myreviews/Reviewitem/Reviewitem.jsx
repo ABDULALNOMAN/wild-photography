6e611ecacb5d94d66beda6be2975032240b470d2
unknown <nomanmd1st2001@gmail.com>
2022-11-10 13:58:58 +0600

    modify myreview

--- src/Component/Page/Myreviews/Reviewitem/Reviewitem.jsx
+++ src/Component/Page/Myreviews/Reviewitem/Reviewitem.jsx
@@ -5,22 +5,19 @@ const Reviewitem = ({ item,handleDelete }) => {
     const { email, photoUrl, name, rating, servvice, text, _id } = item
     
     return (
-        <div className='flex justify-between border bg-sky-700 items-center'>
-            <img className='w-14 h-14 ml-2 rounded-full' src={photoUrl} alt="" />
-            <div>
-                <h4>{name}</h4>
+        <div className='bg-sky-700 shadow-2xl mb-3 relative'>
+            <div className=''>
+                <div className='flex justify-between  items-center w-48 p-3'>
+                    <img className='w-14 h-14 ml-2 rounded-full' src={photoUrl} alt="" />
+                    <div>
+                        <h4 className='text-2xl '>{name}</h4>
+                    </div>
+                </div>
+                <button onClick={() => handleDelete(_id)} className="btn btn-sm mt-2 btn-warning absolute right-2 top-3">delete</button>
             </div>
-            <div>
+            <hr />
+            <div className='p-4'>
                 <p>{text}</p>
-            </div>
-            <div className='my-2 mr-2 flex justify-around items-center'>
-                <div>
-                    <button className="btn btn-sm btn-success">update</button>
-                </div>
-                <div>
-                    <button onClick={() => handleDelete(_id)} className="btn btn-sm mt-2 btn-warning">delete</button>
-                    
-                </div>
             </div>
         </div>
     );
