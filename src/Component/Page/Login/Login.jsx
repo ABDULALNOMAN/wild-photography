c16f24145a75db48282766f48b964609170b3b06
unknown <nomanmd1st2001@gmail.com>
2022-11-10 10:55:52 +0600

    login form style

--- src/Component/Page/Login/Login.jsx
+++ src/Component/Page/Login/Login.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import Helmet from 'react-helmet';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { CallContext } from '../../Contexting/Contexting';
+import loginimg from '../../../assets/undraw_two_factor_authentication_namy.svg';
 
 const Login = () => {
     const { loginUserData, GoogleLoginUsers } = useContext(CallContext)
@@ -34,20 +35,22 @@ const Login = () => {
             <Helmet>
                 <title>servicesDetails</title>
             </Helmet>
-            <div className="bg-sky-700">
-                <div className="grid grid-cols-3 container mx-auto">
-                    <div className='col-span-2'>sdlfjsdfjsdjf</div>
-                    <div className=" border col-span-1 bg-gray-500 p-4">
+            <div className="bg-sky-700 ">
+                <div className="grid grid-cols-3 gap-6 container mx-auto py-10">
+                    <div className='col-span-2'>
+                        <img src={loginimg} alt="" />
+                    </div>
+                    <div className="shadow-lg shadow-slate-900 col-span-1 bg-cyan-600 p-4 ">
                         <form onSubmit={handleLoginUser} className="">
                             <div className="">
                             <label className="label">
-                                <span className="label-text">Email</span>
+                                <span className="">Email</span>
                             </label>
                             <input type="email" placeholder="email" name='email' className="input input-bordered input-info w-full" />
                             </div>
                             <div className="">
                             <label className="label">
-                                <span className="label-text">Password</span>
+                                <span className="">Password</span>
                             </label>
                             <input type="password" placeholder="password" name='password' className="input input-bordered input-info w-full" />
                             <label className="label">
@@ -57,8 +60,13 @@ const Login = () => {
                             <div className="form-control mt-6">
                             <button className="btn btn-primary">Login</button>
                             </div>
-                        </form>
-                        <button onClick={handleGoogleLogin} className='btn btn-info'>google</button>
+                        </form> 
+                        <div className='flex items-center my-3'>
+                            <p className='border w-full'></p>
+                            <p className='mx-3'>or</p>
+                            <p className='border w-full'></p>
+                        </div>
+                        <button onClick={handleGoogleLogin} className='btn btn-info w-full'>google</button>
                     </div>
                 </div>
             </div>
